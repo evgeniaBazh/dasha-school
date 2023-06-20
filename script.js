d3a80fcd840c01323ae53c8252456ca3a9258e72
evgeniaBazh <evgeniabazh7@gmail.com>
2023-06-20 20:04:44 +0500

    changes

--- script.js
+++ script.js
@@ -2,10 +2,13 @@ const navBtns = document.querySelectorAll(".nav__btn");
 
 navBtns.forEach((btn) => {
   const href = btn.href;
+  console.log(btn);
   btn.addEventListener("click", (e) => {
     e.preventDefault();
     const selector = "#" + href.split("#")[1];
+    // убрать id у элемента к которому скроллим
     window.location.hash = selector;
+    // вернуть id
     const block = document.querySelector(selector);
     block.scrollIntoView({
       behavior: "smooth",
